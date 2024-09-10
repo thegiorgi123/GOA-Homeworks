c745ad3e9cfc330c4e291878a414e96e0826ce0f
giorgi <tlashadzegiorgi43@gmail.com>
2024-09-10 20:34:55 +0400

    commit

---
+++ Day 0/lesson1.py
@@ -0,0 +1,15 @@
+print("გამარჯობა!!")
+
+#sees
+print("Goal Oriented Academy")
+"GOA"
+X = "Giorgi"
+"X"
+print(X)
+GOT = "Game Of Thrones" 
+print(GOT)
+print
+
+
+name = "giorgi"
+print(name)
---
+++ Day 1/lesson1.py
@@ -0,0 +1,83 @@
+from turtle import *
+
+#we want to draw a house
+speed(5)
+#step 1: draw a square
+width(8)
+begin_fill()
+color("Purple")
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+end_fill()
+#step 2: draw a door
+begin_fill()
+left(90)
+forward(70)
+color("Green")
+left(90)
+forward(100)
+right(90)
+forward(60)
+right(90)
+forward(100)
+end_fill()
+
+penup()
+
+#step 3: draw a triangle
+goto(200, 200)
+pendown()
+color("yellow")
+
+begin_fill()
+
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+penup()
+
+#step 4: draw first window
+
+goto(0, 0)
+right(150)
+color("brown")
+begin_fill()
+forward(125)
+pendown()
+right(90)
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+end_fill()
+penup()
+
+
+#draw second window
+penup()
+goto(200, 200)
+left(90)
+pendown()
+color("purple")
+forward(25)
+color("brown")
+begin_fill()
+right(90)
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+end_fill()
+
+
+
+exitonclick()
