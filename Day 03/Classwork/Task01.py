a6bf657b00e5ae77495a155a1c0f57305ed2c112
giorgi <tlashadzegiorgi43@gmail.com>
2024-09-12 23:21:50 +0400

    commit

---
+++ Day 03/Classwork/Task01.py
@@ -0,0 +1,14 @@
+name = "giorgi"
+
+print(name)
+population = 2500
+print(type(population))
+print(population * 10)
+print(population + 10)
+print(population - 10)
+print(population / 10)
+print(population // 10)
+print(population ** 10)
+print(population % 10) 
+# ** - ხარისხში აყვანა მაგ: population ** 2
+print(name + name) 
---
+++ Day 03/Homework/homework3.py
@@ -0,0 +1,11 @@
+name = "გიორგი"
+last_name = "ტლაშაძე"
+
+print(name + last_name)
+
+num1 = 1500
+num2 = 300.5
+
+print(num1 + num2)
+print(num1 * num2)
+print(num1 / num2)
