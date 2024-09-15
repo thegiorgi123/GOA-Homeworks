eedd472d0dfe2a2feab7088209d80399d9c1300b
giorgi <tlashadzegiorgi43@gmail.com>
2024-09-15 20:30:25 +0400

    commit

---
+++ Practice/practice.py
@@ -0,0 +1,39 @@
+# name = "გიორგი"
+# school = "ხიდისთავის საჯარო სკოლა"
+# klasi = 11
+# height = 1.7
+# favyy = "ხინკალი"
+
+# print("ჩემი სახელია" + " " + name + ", " + "ჩემი გვარია" + " " + last_name + ", " + "მე ვსწავლობ" + " " + school + "ში, " + "მე ვარ" + " " + str(klasi) + " " + "კლასში, "  + "სიმაღლეში ვარ" + " " + str(height) + ", " + "ჩემი საყვარელი საჭმელია" + " " + favyy + "." ) 
+
+# num1 = 5134
+# num2 = 1724
+# population = 17323
+
+# print(population + num1 - num2)
+# print(num1 + num2 * population)
+# print(population * num2 / num1)
+# print(num1 ** 2 / num2)
+
+# name, age, height, last_name = "giorgi", 16, 1.70, "tlashadze"
+
+# hello = hi= bye = goodbye = 30
+
+
+song = "hello from the other side"
+# print(len(song)) - რამდენი ასოსგან შედგება
+
+# print(song.find("s")) - კონკრეტული ასოს პოვნა
+# print(song.capitalize()) - პირველი სიტყვის პირველი ასოს Capital-ად გადაქევა
+
+# print(song.upper()) - მთლიანი წინადადების Capital letter-ით დაწერა
+
+song2 = "HELLO FROM THE OTHER SIDE"
+
+# print(song2.lower()) - მთლიანი წინადადების დაბალი ასოთი დაწერა
+
+# print(song.count("l")) - წინადადებაში კონკრეტული ასოს რაოდენობის გაგება
+# print(song.replace("l", "L")) - რომელიმე ასოს სხვათი ჩანაცვლება
+# print(song*2, song2*3) - რამოდენიმეჯერ გამეორება
+
+print("my favorite part of song is" + " " + song + ".")
