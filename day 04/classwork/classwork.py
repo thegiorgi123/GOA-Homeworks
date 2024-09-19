c4a4b681a0b9f490c74d64ff620271a184cb2a65
giorgi <tlashadzegiorgi43@gmail.com>
2024-09-19 20:38:21 +0400

    'commit'

---
+++ day 04/classwork/classwork.py
@@ -0,0 +1,55 @@
+# # price = 99
+# # print(price)
+
+# # price = 100
+
+# # print(price)
+
+# level = 14
+
+# print(level)
+
+# level = level + 2
+
+# print(level)
+
+
+
+
+
+num1 = 50
+num2 = 70
+sum = num1 + num2
+print(sum)
+
+price = 50
+price = 60
+
+print(price)
+
+# debbuging არის შეცდომის(ბაგის) გამოსწორება
+print("Hello, World!")
+
+
+greeting = "Good Morning"
+print(greeting)
+
+first_name = "Alice"
+last_name = "Smith"
+print("Name: " + first_name + " " + last_name)
+
+number = 50
+print("The number is: " + str(number))
+
+name = "George"
+print(name)
+
+age = 25
+print("I am" + " " + str(age) + " " + "years old.")
+
+num1 = 10
+num2 = 20
+result = num1 + num2
+print(result)
+
+
