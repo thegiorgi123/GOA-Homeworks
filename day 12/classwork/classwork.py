8d605ce992eb69c823c2c54aa27ed6b548c1ef11
giorgi <tlashadzegiorgi43@gmail.com>
2024-10-24 14:31:46 +0400

    'commit'

--- day 12/classwork/classwork.py
+++ day 12/classwork/classwork.py
@@ -29,7 +29,7 @@
 # i = 1
 # while 1 > 10:
 #     print("Daenerys!")
-
+    
 
 
 # i = 0
---
+++ day 14/classwork/classwork.py
@@ -0,0 +1,29 @@
+# # name = "giorgi"
+# # name += "tlashadze"
+# # print(name)
+
+# name = "Rengoku"
+# res = " "
+# for i in name:
+#     res += i + " "
+# print(res)
+
+
+
+
+# if - (თუ) არის statement - True / False
+# else - false
+# elif როცაა else-ს აღარ კითხულობს. იმ შემთხვევაში თუ elif ჭეშმარიტია
+# if 3 < 4:
+#     print("this is true.")
+# elif 3 < 4:
+#     print("this is still true")
+# else:
+#     print("this is not true")
+
+age = 30
+if age >= 18:
+    print("true")
+else:
+    ("false")
+print("hi")
